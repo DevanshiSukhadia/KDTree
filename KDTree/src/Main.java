b93275eaaad777e4d9a9f05d7391e12081ad4e00
Haasi <haasitha123@gmail.com>
2018-12-16 21:13:27 -0800

    Update Main.java

--- KDTree/src/Main.java
+++ KDTree/src/Main.java
@@ -4,17 +4,24 @@ import java.io.FileReader;
 public class Main {
 
 	public static void main(String[] args) {
-		FileReader file = new FileReader("points.txt");
-                
-       	 	KDTree kdTree = new KDTree();
-        	while (file.read() != -1) {
-            		int x = file.read();
-            		int y = file.read();
-            		Point2D p = new Point2D(x, y);
-            		kdTree.insert(p);
-            		kdTree.search(p);
-            		
-        	}
+		KDTree kdTree = new KDTree();
+		
+		Scanner scanner = null;
+		try {
+			scanner = new Scanner(new File("points.txt"));
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		
+		while(scanner.hasNextInt())
+		{
+			int x = scanner.nextInt();
+		    int y = scanner.nextInt();
+		    Point2D p = new Point2D.Double(x, y);
+		    
+		    System.out.println("Point: " + p.getX() + ", " + p.getY());
+		    kdTree.insert(p);
+		}
        
         	// (50,80),(25,45),(70,75),(55,10),(60,85)
 		
