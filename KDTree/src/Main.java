7fe2d2333cbed52480ebdaf8925f695715b026a4
Haasi <haasitha123@gmail.com>
2018-12-16 19:52:05 -0800

    Update Main.java

--- KDTree/src/Main.java
+++ KDTree/src/Main.java
@@ -2,7 +2,18 @@
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		FileReader file = new FileReader("points.txt");
+                
+       	 	KDTree kdTree = new KDTree();
+        	while (file.read() != -1) {
+            		int x = file.read();
+            		int y = file.read();
+            		Point2D p = new Point2D(x, y);
+            		kdTree.insert(p);
+        	}
+       
+        	// (50,80),(25,45),(70,75),(55,10),(60,85)
+		
 		Rect r = new Rect();
 		r.drawRect();
 
